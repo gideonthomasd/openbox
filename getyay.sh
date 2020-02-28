a83623dcf5ac2ada80cec5b566c51abf1f871e16
gideonthomasd <56291811+gideonthomasd@users.noreply.github.com>
2020-02-28 23:48:45 +0000

    Update getyay.sh

--- getyay.sh
+++ getyay.sh
@@ -4,10 +4,10 @@ set -e
 sudo pacman -Syyu --noconfirm 
 git clone https://aur.archlinux.org/yay.git
 cd yay
-makepkg -si
-yay -S oblogout
-yay -S pamac-aur
-yay -S ttf-zekton-rg
-yay -S obmenu-generator
-yay -S conky-lua-archers
-yay -S ttf-spacemono
+makepkg -si --noconfirm
+yay -S --noconfirm oblogout
+yay -S --noconfirm pamac-aur
+#yay -S ttf-zekton-rg
+yay -S --noconfirm obmenu-generator
+yay -S --noconfirm conky-lua-archers
+#yay -S ttf-spacemono
