ff7b60b46d639acc74a44a2806986052ae5ffc8c
gideonthomasd <56291811+gideonthomasd@users.noreply.github.com>
2020-02-28 23:46:15 +0000

    Update InitialSetup.sh

--- InitialSetup.sh
+++ InitialSetup.sh
@@ -3,18 +3,19 @@ set -e
 
 
 sudo pacman -Syyu --noconfirm
-sudo pacman -S menumaker lxsession lxappearance arc-gtk-theme xarchiver xterm base-devel nitrogen firefox git geany
-sudo pacman -S lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings pcmanfm
-sudo pacman -S networkmanager network-manager-applet tint2
-sudo pacman -S xorg-server xorg xorg-xinit
-sudo pacman -S openbox obconf
-sudo pacman -S moka-icon-theme
-sudo pacman -S w3m
-sudo pacman -S pavucontrol pulseaudio pulseaudio-equalizer
-sudo pacman -S gmrun
-sudo pacman -S neofetch rofi gtk2-perl lsb-release
-sudo pacman -S adapta-gtk-theme gvfs ntfs-3g
-sudo pacman -S virtualbox-guest-utils
+sudo pacman -S menumaker lxsession lxappearance arc-gtk-theme xarchiver xterm base-devel nitrogen firefox git geany --noconfirm
+#sudo pacman -S lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings pcmanfm
+sudo pacman -S pcmanfm thunar networkmanager network-manager-applet tint2 --noconfirm
+sudo pacman -S xorg-server xorg xorg-xinit --noconfirm
+sudo pacman -S openbox obconf man-db --noconfirm
+sudo pacman -S moka-icon-theme --noconfirm
+sudo pacman -S w3m unzip --noconfirm
+sudo pacman -S pavucontrol pulseaudio pulseaudio-equalizer --noconfirm
+sudo pacman -S gmrun --noconfirm
+sudo pacman -S neofetch rofi gtk2-perl lsb-release --noconfirm
+sudo pacman -S adapta-gtk-theme gvfs ntfs-3g --noconfirm
+sudo pacman -S lxterminal xterm xfce4-terminal --noconfirm
+#sudo pacman -S virtualbox-guest-utils
 sudo systemctl start NetworkManager
 sudo systemctl enable NetworkManager
-sudo systemctl enable lightdm
+#sudo systemctl enable lightdm
