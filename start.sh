8d42bb690b9c603bc2f511be3ad8bfb4b055fc82
gideonthomasd <56291811+gideonthomasd@users.noreply.github.com>
2020-02-29 20:45:21 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,12 @@
 chmod +x InitialSetup.sh
 chmod +x getyay.sh
+chmod +x call.sh
+chmod +x cbunsen.sh
+chmod +x celegant.sh
+chmod +x cfofo.sh
+chmod +x clua.sh
+chmod +x cnemesis.sh
+chmod +x cnormal.sh
 ./InitialSetup.sh
 ./getyay.sh
 
