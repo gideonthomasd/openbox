83e0fb909fafaf15f772b80bcfcaa479cc2e7e92
gideonthomasd <56291811+gideonthomasd@users.noreply.github.com>
2020-03-08 14:12:02 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -45,6 +45,12 @@ cd conky
 cp -r * ~/.config/conky
 cd ~/openbox
 
+[ -d $HOME"/.conky" ] || mkdir -p $HOME"/.conky"
+
+cd conkymanager
+cp -r * ~/.conky
+cd ~/openbox
+
 [ -d $HOME"/.config/openbox" ] || mkdir -p $HOME"/.config/openbox"
 cp autostart ~/.config/openbox/autostart
 cp menu.xml ~/.config/openbox/menu.xml
