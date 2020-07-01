57f8cd4b0856b362e3dbf3587e68a8f19eeb8955
gideonthomasd <56291811+gideonthomasd@users.noreply.github.com>
2020-07-01 15:19:14 +0100

    Update InitialSetup.sh

--- InitialSetup.sh
+++ InitialSetup.sh
@@ -15,6 +15,8 @@ sudo pacman -S gmrun arandr gsimplecal --noconfirm
 sudo pacman -S neofetch rofi gtk2-perl lsb-release --noconfirm
 sudo pacman -S adapta-gtk-theme gvfs ntfs-3g --noconfirm
 sudo pacman -S lxterminal xterm xfce4-terminal jgmenu gtk2-perl conky-manager xfce4-appfinder --noconfirm
+# Polybar -useful
+sudo pacman -S pacman-contrib termite awesome-terminal-fonts ttf-font-awesome noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra --noconfirm
 #sudo pacman -S virtualbox-guest-utils
 sudo systemctl start NetworkManager
 sudo systemctl enable NetworkManager
