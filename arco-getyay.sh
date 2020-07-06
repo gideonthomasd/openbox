8dd11a3242e5bf676f517c04a4f691095e768047
gideonthomasd <56291811+gideonthomasd@users.noreply.github.com>
2020-07-06 08:36:43 +0100

    Create arco-getyay.sh

---
+++ arco-getyay.sh
@@ -0,0 +1,14 @@
+sudo pacman -Syyu --noconfirm 
+#git clone https://aur.archlinux.org/yay.git
+#cd yay
+#makepkg -si --noconfirm
+# yay -S --noconfirm oblogout
+# Often problems yay -S --noconfirm pamac-aur
+#yay -S ttf-zekton-rg
+yay -S --noconfirm obmenu-generator
+#yay -S --noconfirm ttf-mononoki
+yay -S --noconfirm mintstick-git
+yay -S --noconfirm polybar
+#yay -S --noconfirm conky-lua-archers
+#yay -S ttf-spacemono
+
